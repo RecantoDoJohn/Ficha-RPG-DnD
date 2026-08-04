6e5de54bc7cb3ddb1a62b600b7f73310d0fc4d85
João Manuel <johnjohn200513@gmail.com>
2026-08-03 22:51:21 -0300

    feat(relation): adiciona relacionamento entre mesa e jogador

--- src/main/java/recanto/john/rpg_api/domain/mesa/Mesa.java
+++ src/main/java/recanto/john/rpg_api/domain/mesa/Mesa.java
@@ -4,10 +4,13 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import recanto.john.rpg_api.domain.jogador.Jogador;
 import recanto.john.rpg_api.domain.personagem.Personagem;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Table(name = "Mesas")
 @Entity(name = "Mesa")
@@ -24,6 +27,9 @@ public class Mesa {
 
     @OneToMany(mappedBy = "mesa", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Personagem> personagems = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "mesas")
+    private Set<Jogador> jogadores = new HashSet<>();
 
     public Mesa(@Valid DadosCadastroMesa dados) {
         this.titulo = dados.titulo();
