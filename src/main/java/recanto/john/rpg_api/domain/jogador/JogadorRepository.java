6bbe4ea31a469689ff1a88d4ad571ff64c9baf96
João Manuel <johnjohn200513@gmail.com>
2026-08-03 22:50:42 -0300

    feat(repository): adiciona o repository do jogador

---
+++ src/main/java/recanto/john/rpg_api/domain/jogador/JogadorRepository.java
@@ -0,0 +1,6 @@
+package recanto.john.rpg_api.domain.jogador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JogadorRepository extends JpaRepository<Jogador, Long> {
+}
