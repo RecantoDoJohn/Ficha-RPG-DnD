fce0b0e3996d3cd485a4b8bbb06bb0638209c0c0
João Manuel <johnjohn200513@gmail.com>
2026-08-07 11:43:46 -0300

    feat(controller): adiciona controller para rotas de paginas HTML

---
+++ src/main/java/recanto/john/rpg_api/controller/WebController.java
@@ -0,0 +1,18 @@
+package recanto.john.rpg_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/ficha")
+    public String ficha() {
+        return "fichaPlayer";
+    }
+
+    @RequestMapping("/mestre")
+    public String mestre() {
+        return "painelMestre";
+    }
+}
